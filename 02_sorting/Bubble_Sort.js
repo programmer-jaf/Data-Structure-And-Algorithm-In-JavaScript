d18e7ab64a0e0dd5c3328065e04f60596523014b
Programmer-Jaf <programmerjaf@gmail.com>
2025-09-15 12:16:48 +0600

    Solve Bubble Sort with practice question

---
+++ 02_sorting/Bubble_Sort.js
@@ -0,0 +1,21 @@
+// GFG Link --> https://www.geeksforgeeks.org/problems/bubble-sort/1
+
+// Time Complexity --> O(n^2)
+// Space Complexity --> O(1)
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// Test Case
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log(bubbleSort(arr));
